11b9ffb3466670adccdbf1159b84f86237b81637
MaSKoH <troianow@stanford.edu>
2021-03-12 08:46:29 -0600

    added support for replace ext

--- scripts/translate-nb.py
+++ scripts/translate-nb.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 import argparse
+import logging
 import os
-import nbtranslate
 from nbtranslate import core
+
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -10,12 +12,14 @@ def main():
     parser.add_argument('-i', '--input', required=True)
     parser.add_argument('-o', '--output')
     parser.add_argument('--overwrite', action='store_true')
+    parser.add_argument('--replace_ext', action='store_true')
     parsed = parser.parse_args()
     input_file = parsed.input
     output_file = parsed.output
     overwrite = parsed.overwrite
+    replace_ext = parsed.replace_ext
     dirname = os.path.dirname(input_file)
-    ext, name = os.path.splitext(os.path.basename(input_file))
+    name, ext = os.path.splitext(os.path.basename(input_file))
     if ext not in ['.py', '.ipynb']:
         raise ValueError(f"The input's extension must end in `py` or `ipynb`. Got '{input_file}' instead")
     elif ext == '.py':
@@ -26,8 +30,10 @@ def main():
     if output_file is None:
         output_file = os.path.join(dirname, f'{name}{new_ext}')
     else:
-        out_ext, _ = os.path.splitext(output_file)
-        if out_ext != new_ext:
+        base_output_file, out_ext = os.path.splitext(output_file)
+        if replace_ext:
+            output_file = base_output_file + new_ext
+        elif out_ext != new_ext:
             raise ValueError(f"Since the input's extension is {ext}, the output's must be {new_ext}")
 
     if os.path.exists(output_file) and not overwrite:
